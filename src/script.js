d77d6f19516b321e508708fa183662367f3219a4
Ashwin-P-S <ashwinsiva54@gmail.com>
2022-08-03 18:34:29 +0530

    chore: Error/Exception handling

--- src/script.js
+++ src/script.js
@@ -6,23 +6,26 @@ let fetchApi = async (url, location) => {
     await fetch(url)
     .then((response) => { 
 
-        if (response.ok) return response.json()
+        if (response.ok) return response.json();
     })
     .then((data) => {
         
-        temperature = (data["main"]["temp"]-273.15).toPrecision(4)
-        humidity = data["main"]["humidity"]
-        description = data["weather"][0]["description"]
-        windSpeed = data["wind"]["speed"]
+        temperature = (data["main"]["temp"]-273.15).toPrecision(4);
+        feelsLike = (data["main"]["feels_like"]-273.15).toPrecision(4);
 
-        showData(temperature, humidity, description, windSpeed, location)
+        humidity = data["main"]["humidity"];
+        description = data["weather"][0]["description"];
+        windSpeed = data["wind"]["speed"];
+        pressure = data["main"]["pressure"];
+        visibility = (data["visibility"])/1000;
+
+        showData(temperature, feelsLike,  humidity, description, windSpeed, location, pressure, visibility);
 
     })
     .catch(() => {
 
-        alert("Please Enter Valid Location!");
-        clearData()
-    })
+        catchError("Error: City not found");
+    });
 
 }
 
@@ -30,42 +33,46 @@ let getWeather = () => {
 
     var location = document.getElementById('location').value;
     
-    let apiKey = '63fcf6ac9a00124665e8461cfe2370bb'
+    let apiKey = '63fcf6ac9a00124665e8461cfe2370bb';
 
     let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + apiKey;
 
-    if (location === "") {
-
-        alert("Please Enter Location!")
-        clearData()
-    } else {
-        fetchApi(url, location)
-    }
+    location === "" ? catchError("Warning: please enter location") : fetchApi(url, location);
 
 }
 
 let titleCase = (str) => {
+
     return str.toLowerCase().replace(/\b\w/g, s => s.toUpperCase());
+}
+
+let catchError = (errorMsg) => {
+
+    clearData();
+    document.getElementById('description').innerHTML = errorMsg;
 }
 
 let clearData = () => {
 
-    document.getElementById('location').value = null
-    document.getElementById('locate').innerHTML = null
-    document.getElementById('temperature').innerHTML = null
-    document.getElementById('description').innerHTML = null
-    document.getElementById('humidity').innerHTML = null
-    document.getElementById('windSpeed').innerHTML = null
-
+    document.getElementById('location').value = null;
+    document.getElementById('locate').innerHTML = null;
+    document.getElementById('temperature').innerHTML = null;
+    document.getElementById('description').innerHTML = null;
+    document.getElementById('humidity').innerHTML = null;
+    document.getElementById('windSpeed').innerHTML = null;
+    document.getElementById('feels_like').innerHTML = null;
+    document.getElementById('pressure').innerHTML = null;
+    document.getElementById('visibility').innerHTML = null;
 }
 
-let showData = (temperature, humidity, description, windSpeed, location) => {
+let showData = (temperature, feelsLike, humidity, description, windSpeed, location, pressure, visibility) => {
 
-    document.getElementById('locate').innerHTML = titleCase(location)
-    document.getElementById('temperature').innerHTML = "Temperature : " + temperature + " °C"
-    document.getElementById('description').innerHTML = titleCase(description)
-    document.getElementById('humidity').innerHTML = "Humidity : " + humidity + " %"
-    document.getElementById('windSpeed').innerHTML = "Wind Speed : " + windSpeed + " m/s"
-
+    document.getElementById('locate').innerHTML = titleCase(location);
+    document.getElementById('description').innerHTML = titleCase(description);
+    document.getElementById('temperature').innerHTML = "Temperature : " + temperature + " °C";
+    document.getElementById('feels_like').innerHTML = "Feels Like : " + feelsLike + " °C";
+    document.getElementById('humidity').innerHTML = "Humidity : " + humidity + " %";
+    document.getElementById('windSpeed').innerHTML = "Wind Speed : " + windSpeed + " m/s";
+    document.getElementById('pressure').innerHTML = "Pressure : " + pressure + " hPa";
+    document.getElementById('visibility').innerHTML = "Visibility : " + visibility + " Km";
 }
-
